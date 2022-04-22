d6896a80b15f11c02e6038f02412b6a67bb860cd
tanzhijin <tanzhijin@oray.com>
2022-04-22 10:58:09 +0800

    hotfix:更新豆瓣音乐250爬取

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ list_movie = []
 headers = {'User-Agent': str(UserAgent().random)}
 
 # 爬取的内容保存在csv文件中
-f = open(r"C:\Users\tanzhijingg\Desktop\test2.csv", "w+", newline='', encoding="gb18030")
+f = open(r"C:\Users\tanzhijingg\Desktop\douban_music.csv", "w+", newline='', encoding="gb18030")
 writer = csv.writer(f, dialect='excel')
 # csv文件中第一行写入标题
-writer.writerow(["name", "year", "mark", "comment", "quote"])
+writer.writerow(["song", "singer", "time", "liupai", "mark", "comment"])
 
 
 # 定义爬取内容的函数
@@ -23,22 +23,18 @@ def music_info(url):
     html = requests.get(url, headers=headers)
     selector = etree.HTML(html.text)
 
-    infomation = selector.xpath('//div[@class="item"]')
+    infomation = selector.xpath('//tr[@class="item"]')
     for info in infomation:
-        name = info.xpath('div[2]/div[1]/a/span[1]/text()')[0].strip()
-        year = int(info.xpath('div[2]/div[2]/p[1]/text()')[1].strip()[0:4])
-
-        mark = float(info.xpath('div[2]/div[2]/div/span[2]/text()')[0].strip())
+        song = info.xpath('td[2]/div/a/text()')[0].strip()
+        singer = info.xpath('td[2]/div/p/text()')[0].split("/")[0]
+        times = info.xpath('td[2]/div/p/text()')[0].split("/")[1]
+        liupai = info.xpath('td[2]/div/p/text()')[0].split("/")[-1]
+        mark = float(info.xpath('td[2]/div/div/span[2]/text()')[0].strip())
         comment = int(
-            info.xpath('div[2]/div[2]/div/span[4]/text()')[0].strip().strip("(").strip(")").replace('人评价', '').strip())
-        # noinspection PyBroadException
-        try:
-            quote = info.xpath('div[2]/div[2]/p[@class="quote"]/span/text()')[0].strip()
-        except:
-            quote = ""
+            info.xpath('td[2]/div/div/span[3]/text()')[0].strip().strip("(").strip(")").replace('人评价', '').strip())
 
-        list_info = [name, year, mark, comment, quote]
-        writer.writerow([name, year, mark, comment, quote])
+        list_info = [song, singer, times, liupai, mark, comment]
+        writer.writerow([song, singer, times, liupai, mark, comment])
         list_movie.append(list_info)
 
     # 防止请求频繁，故睡眠1秒
@@ -46,20 +42,20 @@ def music_info(url):
 
 
 if __name__ == '__main__':
-    urls = ['https://movie.douban.com/top250?start={}'.format(str(i)) for i in range(0, 250, 25)]
+    urls = ['https://music.douban.com/top250?start={}'.format(str(i)) for i in range(0, 250, 25)]
     # 调用函数music_info循环爬取内容
     for url in tqdm(urls):
         music_info(url)
     # 关闭csv文件
     f.close()
     # 爬取的内容保存在xls文件中
-    header = ["name", "year", "mark", "comment", "quote", ]
+    header = ["song", "singer", "time", "liupai", "mark", "comment"]
 
-    #设置列头加粗
+    # 设置列头加粗
     font = xlwt.Font()
     font.bold = True
 
-    #设置列头单元格居中
+    # 设置列头单元格居中
     alignment = xlwt.Alignment()
     alignment.horz = 0x02
 
@@ -80,12 +76,13 @@ if __name__ == '__main__':
             sheet.write(i, j, data)
             j += 1
         i += 1
-    #设置单元格宽度
+    # 设置单元格宽度
     sheet.col(0).width = 256 * 30
-    sheet.col(1).width = 256 * 10
+    sheet.col(1).width = 256 * 20
     sheet.col(2).width = 256 * 10
     sheet.col(3).width = 256 * 15
-    sheet.col(4).width = 256 * 60
+    sheet.col(4).width = 256 * 10
+    sheet.col(5).width = 256 * 10
 
     # 保存文件
-    book.save('doubanmovie.xls')
+    book.save('douban_music.xls')
